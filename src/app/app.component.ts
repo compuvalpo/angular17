f6f090d1727fcef602810f5f780e441be336533a
compuvalpo@gmail.com <compuvalpo@gmail.com>
2024-04-23 23:56:00 -0400

    Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatCardModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
